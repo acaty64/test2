c33fd915cbb66a6ad2f0cf5adb43a3812292a3e0
PCUcss <acaty2@gmail.com>
2025-07-25 13:29:06 -0500

    Dusk with errors

---
+++ app/tests/Browser/DuskTestCase.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Laravel\Dusk\Tests\Browser;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Support\Collection;
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\TestCase;
+use Orchestra\Testbench\Concerns\CreatesApplication;
+use PHPUnit\Framework\Attributes\BeforeClass;
+
+class DuskTestCase extends TestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Determine the application's base URL.
+     *
+     * @return string
+     */
+    protected function baseUrl()
+    {
+        return rtrim(config('app.url'), '/');
+    }
+
+    /**
+     * Register the base URL with Dusk.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Browser::$storeScreenshotsAt = __DIR__.'/screenshots';
+        Browser::$storeConsoleLogAt = __DIR__.'/console';
+        Browser::$storeSourceAt = __DIR__.'/source';
+    }
+
+    /**
+     * Prepare for Dusk test execution.
+     */
+    #[BeforeClass]
+    public static function prepare(): void
+    {
+        if (! static::runningInSail()) {
+            static::startChromeDriver();
+        }
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     */
+    protected function driver(): RemoteWebDriver
+    {
+        $options = (new ChromeOptions)->addArguments(collect([
+            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+            '--disable-search-engine-choice-screen',
+            '--disable-smooth-scrolling',
+        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
+            return $items->merge([
+                '--disable-gpu',
+                '--headless=new',
+            ]);
+        })->all());
+
+        #$options->setBinary('/vendor/laravel/dusk/bin/chromedriver-linux');
+
+        return RemoteWebDriver::create(
+            $_ENV['DUSK_DRIVER_URL'] ?? 'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+        );
+    }
+
+    /**
+     * Determine whether the Dusk command has disabled headless mode.
+     */
+    protected function hasHeadlessDisabled(): bool
+    {
+        return isset($_SERVER['DUSK_HEADLESS_DISABLED']) ||
+               isset($_ENV['DUSK_HEADLESS_DISABLED']);
+    }
+
+    /**
+     * Determine if the browser window should start maximized.
+     */
+    protected function shouldStartMaximized(): bool
+    {
+        return isset($_SERVER['DUSK_START_MAXIMIZED']) ||
+               isset($_ENV['DUSK_START_MAXIMIZED']);
+    }
+}
